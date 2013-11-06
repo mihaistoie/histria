b1a4ce7b41b71fdb7240c579cc9a0109a5354491
Catalin Vascu <ccaavvaa@gmail.com>
2013-11-06 13:56:31 +0100

    delete usings

--- core/src/Framework/BaseObject.cs
+++ core/src/Framework/BaseObject.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Sikia.Framework
+﻿namespace Sikia.Framework
 {
     public abstract class BaseObject
     {
