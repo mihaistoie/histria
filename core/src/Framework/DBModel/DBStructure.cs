738a2b23533a4c580f0bc6f3b5ae96f8695311a1
mihaistoie <mihaistoie@gmail.com>
2013-11-12 20:53:29 +0100

    #1 database structure in progress

---
+++ core/src/Framework/DBModel/DBStructure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+using Sikia.DataBase;
+
+namespace Sikia.Framework.DBModel
+{
+    public class DBStructure
+    {
+        public Dictionary<string, DBTable> Tables = new Dictionary<string, DBTable>();
+        public void LoadFromDataBase(SqlConnection connection, DatabaseTranslator translator)
+        {
+            /* */
+        
+        }
+        
+    }
+}
