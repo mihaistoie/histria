52d5d8e8258a322d4f475053e4ae89b4212505ae
mihaistoie <mihaistoie@gmail.com>
2013-12-15 17:51:25 +0100

    #1

--- core/src/DataTypes/BelongsTo.cs
+++ core/src/DataTypes/BelongsTo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Sikia.src.DataTypes
+namespace Sikia.DataTypes
 {
     public class BelongsTo<T>
     {
--- core/src/DataTypes/HasOne.cs
+++ core/src/DataTypes/HasOne.cs
@@ -10,3 +10,5 @@ namespace Sikia.DataTypes
 
     }
 }
+
+
