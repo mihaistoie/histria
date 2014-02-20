3bfb0bdb7e7a3073db027dcd8e20bca98f6c2b42
mihai <mihaistoie@gmail.com>
2014-02-20 17:37:46 +0100

    #1

--- src/Sikia.Model/Attributes/NoModelAttribute.cs
+++ src/Sikia.Model/Attributes/NoModelAttribute.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace Sikia.Model
 {
-  /// <summary>
-    /// This class is ignored  
+    /// <summary>
+    /// This class is ignored  by the model loader
     /// </summary>
     [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
     public class NoModelAttribute : System.Attribute
---
+++ src/Sikia.Sys/StringFormatType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sikia
+{
+    public enum StringFormatType
+    {
+        None, Email, Web, Password, Phone
+    }
+}
