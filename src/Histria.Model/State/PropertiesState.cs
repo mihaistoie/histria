8ec7052a02647d26767165cf998010d421d27710
mihaistoie <mihaistoie@gmail.com>
2014-03-11 11:47:46 +0100

    #15 Object state in progress

---
+++ src/Histria.Model/State/PropertiesState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Histria.Model
+{
+    public class PropertiesState
+    {
+        protected virtual BaseItemState GetItemByName(string key) 
+        {
+            return null;
+        }
+
+        protected virtual BaseItemState GetItemByIndex(int index)
+        {
+            return null;
+        }
+
+        public  BaseItemState this[string key]
+        {
+            get
+            {
+                return GetItemByName(key);
+            }
+        }
+
+        public  BaseItemState this[int index]
+        {
+            get
+            {
+                return GetItemByIndex(index);
+            }
+        }
+    }
+}
