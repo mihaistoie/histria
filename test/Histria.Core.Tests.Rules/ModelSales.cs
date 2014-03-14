9c261e53c412db3517b2595b7f656adba13165be
mihaistoie <mihaistoie@gmail.com>
2014-03-14 14:26:18 +0100

    #17 documentation

---
+++ test/Histria.Core.Tests.Rules/ModelSales.cs
@@ -0,0 +1,27 @@
+﻿using Histria.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Histria.Core.Tests.Rules.Customers
+{
+    public class SalesOrder : InterceptedObject
+    {
+        public virtual string Customer { get; set; }
+        public virtual int OrderNumber { get; set; }
+        public virtual DateTime OrderDate { get; set; }
+        [Association(Relation.Composition, Inv="Order")]
+        public virtual HasMany<OrderLine> Lines { get; set; }
+    }
+
+    public class OrderLine : InterceptedObject
+    {
+        public virtual string Product { get; set; }
+        public virtual Decimal DecQty { get; set; }
+        public virtual Decimal UnitPrice { get; set; }
+        public virtual Decimal Price { get; set; }
+        [Association(Relation.Composition, Inv = "Lines")]
+        public virtual BelongsTo<SalesOrder> Order { get; set; }
+    }
+}
