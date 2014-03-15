f798dbee71aee417099a86a2b68c5e5880f603c2
mihaistoie <mihaistoie@gmail.com>
2014-03-15 06:50:20 +0100

    #

---
+++ src/Histria.Core/Rules/RulesCallStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Histria.Core.Rules
+{
+    class RulesCallStack
+    {
+        public void Push(IInterceptedObject instance, string property)
+        {
+
+        }
+        //Pop
+        //Clear
+
+    }
+}
