319b3870c5f0e03ce228ad952926d1dbd85e6b7b
mihaistoie <mihaistoie@gmail.com>
2015-02-24 21:04:41 +0100

    #dbmodel generator unit tests list

---
+++ test/Histria.Model.Db.Tests/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Histria.DbModel.Tests
+{
+    public class BaseModel : IClassModel
+    {
+        public virtual Guid Uuid { get; set; }
+    }
+
+
+    public class Fruit : BaseModel, IPersistentObj
+    {
+    }
+}
--- test/Histria.Model.Db.Tests/ModelDbTests.cs
+++ test/Histria.Model.Db.Tests/ModelDbTests.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Histria.Json;
 using Histria.Sys;
+using Histria.Db;
+using Histria.Db.Model;
+using Histria.DbModel.Tests;
 
 namespace Histria.Model.Db.Tests
 {
@@ -10,21 +13,93 @@ namespace Histria.Model.Db.Tests
     {
 
         private static ModelManager model;
-        static string sqlServerInstance = "(local)\\SQLEXPRESS";
+        private static DbSchema schema;
+        //static string sqlServerInstance = "(local)\\SQLEXPRESS";
   
         [ClassInitialize]
         public static void Setup(TestContext testContext)
         {
-            JsonObject cfg = (JsonObject)JsonValue.Parse(@"{""nameSpaces"": [""Customers""]}");
+            JsonObject cfg = (JsonObject)JsonValue.Parse(@"{""nameSpaces"": [""DbModel""]}");
             model = ModelManager.LoadModel(cfg);
             ModulePlugIn.Load("Histria.Db.MsSql");
             ModulePlugIn.Initialize(null);
+            schema = model.Schema(DbProtocol.mssql);
         }
 
         [TestMethod]
-        public void ColumnsStrings()
+        public void TableFound()
+        {
+            //simple table
+            Assert.AreEqual(true, schema.ContainsTable(typeof(Fruit).Name), "Table found");
+            //derived classes with single table
+            //derived classes with more tables
+        }
+        [TestMethod]
+        public void Primarykeys()
+        {
+            //Assert.AreEqual(true, schema.ContainsTable(typeof(Fruit).Name), "Table found");
+            //uid
+            //code
+            //multi key
+        }
+        [TestMethod]
+        public void StringColumns()
+        {
+            //max length , requred
+        }
+        [TestMethod]
+        public void IntegerColumns()
+        {
+            //int, bigint ... short
+        }
+        [TestMethod]
+        public void EnumColumns()
+        {
+            // enum as smallint
+            // enum as string
+        }
+
+        [TestMethod]
+        public void NumbersColumns()
         {
         }
+
+        [TestMethod]
+        public void DateTimeColumns()
+        {
+        }
+
+        [TestMethod]
+        public void NullablesColumns()
+        {
+        }
+
+        [TestMethod]
+        public void ConstraintsColumns()
+        {
+            //required ...
+        }
+
+        [TestMethod]
+        public void CompositeTypesColumns()
+        {
+            //binary
+            //memo
+        }
+
+
+        [TestMethod]
+        public void ForeingKeys()
+        {
+            //multiple
+        }
+
+
+        [TestMethod]
+        public void Indexes()
+        {
+        }
+
     }
 }
 
